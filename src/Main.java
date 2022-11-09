6f89eaae802b019cbfca1aa743ba7c8c11069c67
Aizada01 <aizada.0206@mail.ru>
2022-11-09 19:42:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] array = new Integer[]{1, 3, -2, 5, -7, 10, 8};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }}
